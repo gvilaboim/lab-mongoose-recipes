92a65c7308e55111a6497f6bb76069c9e577873c
Gonçalo Vilaboim <gonvipa@gmail.com>
2023-02-09 20:31:28 +0000

    Finish I: 1  | 2

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
-
+mongoose.set('strictQuery', true);
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
@@ -17,6 +17,17 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    Recipe.create({
+      title: "Cozido",
+      level: "UltraPro Chef",
+      ingredients: [ "pasta" , "tomatoes"],
+      cuisine : "anything",
+      dishType : "main_course",
+      duration : 15,
+      creator : "Gonçalo & Yinong"
+      }).then(newRecipe => {
+      console.log("Recipe inserted!!")
+  })
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
